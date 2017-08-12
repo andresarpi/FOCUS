0dd445eb141c35647eba14c91944b34dc11761bc
Andres Arpi <aarpi@itba.edu.ar>
2017-08-12 20:25:01 +0000

    Generate survey model and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812195646) do
+ActiveRecord::Schema.define(version: 20170812202036) do
+
+  create_table "surveys", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at"
+    t.integer "feeling"
+    t.integer "focus"
+    t.integer "activity_id"
+    t.index ["user_id"], name: "index_surveys_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
