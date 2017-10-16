1d23f77b7858ed80cfa264665e71a92a3e4c19fa
Andres Arpi <aarpi@itba.edu.ar>
2017-10-16 14:54:41 -0300

    Start defining the tests for the rake tasks.

---
+++ test/integration/rake_tasks_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class RakeTasksTest < ActionDispatch::IntegrationTest
+  include FactoryGirl::Syntax::Methods
+
+  def setup
+    Rails.application.load_tasks
+    @user = create(:user)
+    @config = build(:user_config_bsas)
+    @config.user_id = @user.id
+    @config.save
+
+    @user_survey_chedule = UserSurveySchedule.create(user_id: @user.id, scheduled_time: get_float_now)
+
+  end
+
+  ###########     Survey_scheduling       ##############################
+  test 'generate UserSurveySchedule with clean up' do
+    Rake::Task['survey_scheduling:generate_schedule_with_clean_up'].invoke
+    count = UserSurveySchedule.count
+    assert count == @user.config.times  
+  end
+
+  
+  test 'clean up existing UserSurveySchedules' do
+    debugger
+    Rake::Task['survey_scheduling:clean_up'].invoke
+    count = UserSurveySchedule.count
+    assert count == 0
+  end
+
+  private
+  def get_float_now
+      t = Time.current
+      minutes = t.min
+      if (minutes < 30)
+          minutes = 0.0
+      else
+          minutes = 0.5
+      end
+      return t.hour+minutes
+  end
+
+end
